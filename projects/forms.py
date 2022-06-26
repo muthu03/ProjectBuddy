a72f3bce3e118c0c26c4ad3cea9190912d19c650
muthu131220 <104063713+muthu131220@users.noreply.github.com>
2022-06-26 23:08:05 +0530

    added static files

--- projects/forms.py
+++ projects/forms.py
@@ -1,8 +1,21 @@
+import imp
+from tkinter.ttk import Widget
 from django.forms import ModelForm
 from .models import Project
-
+from django import forms
 
 class projectForm(ModelForm):
     class Meta:
         model=Project
         fields=['title','description','featured_image','demo_link','source_link','tags']
+        widgets={
+            'tags':forms.CheckboxSelectMultiple(),
+        }
+    def __init__(self,*args,**kwargs):
+        super(projectForm,self).__init__(*args,**kwargs)
+        
+        for name,field in self.fields.items():
+            field.widget.attrs.update({'class':'input'})
+
+
+        
